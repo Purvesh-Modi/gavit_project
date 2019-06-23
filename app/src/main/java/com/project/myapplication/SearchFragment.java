230b9bf0f523c9b54b1e0cf9cc2e018def5e2dd1
Purvesh Modi <purveshmodi5@gmail.com>
2019-06-23 19:50:33 +0530

    Minor layout changes

--- app/src/main/java/com/project/myapplication/SearchFragment.java
+++ app/src/main/java/com/project/myapplication/SearchFragment.java
@@ -4,21 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
 
-    public static final String TAG = "SearchFragment";
-    View view;
+    public static final String       TAG = "SearchFragment";
+    private             View         view;
+    private             RecyclerView mRvSearchRecycler;
 
-    public SearchFragment() {
-
-    }
+    public SearchFragment() {}
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Nullable
@@ -31,7 +39,12 @@ public class SearchFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mRvSearchRecycler = view.findViewById(R.id.rv_search_recycler);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.action_search, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
